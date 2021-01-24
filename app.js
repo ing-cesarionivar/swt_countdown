48c8262274507491942855858f82ee5a95904c6f
Cesario Nivar <ing.cesarionivar@gmail.com>
2021-01-24 16:50:12 -0400

    Countdown Functionality ready

---
+++ app.js
@@ -0,0 +1,39 @@
+const countdownBox = document.querySelector('.countdown-box');
+const daysEl = document.querySelector('.days');
+const hoursEl = document.querySelector('.hours');
+const minutesEl = document.querySelector('.minutes');
+const secondsEl = document.querySelector('.seconds');
+
+
+function timer(date) {
+
+    setInterval(() => {
+
+        const countdown = new Date(date).getTime();
+
+        const now = new Date().getTime();
+
+        const difference = countdown - now;
+        
+
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / (1000));
+
+        daysEl.innerHTML = `${days} <br><span>Días</span>`;
+        hoursEl.innerHTML = `${hours}<br><span>Horas</span>`;
+        minutesEl.innerHTML = `${minutes}<br><span>Minutos</span>`;
+        secondsEl.innerHTML = `${seconds}<br><span>Segundos</span>`;
+
+        if (difference < 0) {
+            clearInterval(timer);
+            countdownBox.classList.add('timeOut');
+            countdownBox.innerHTML = ` <h4>It's Summer Work And Travel Time! <br>🥳📢</h4>`;
+        }
+
+
+    }, 1000);
+}
+
+timer('May 1, 2021 24:00:00');
